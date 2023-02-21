83de625c508e8aeac594ac39e3dc951ce9031fa4
ElonMuskRat420xx <gnelson6842@gmail.com>
2023-02-21 12:56:10 +0000

    21st Feb 12:56PM

--- src/components/Main/Education/Education.jsx
+++ src/components/Main/Education/Education.jsx
@@ -104,13 +104,7 @@ function EducationSection() {
       {/* Education Section NESCOL */}
 
       <div className="pt-2">
-        <Image
-          priority="true"
-          src={Nescol}
-          alt="NESCOL"
-          className="w-[4rem]"
-
-/>
+        <Image priority="true" src={Nescol} alt="NESCOL" className="w-[4rem]" />
 
         <div className="w-full py-2 ">
           <div className="flex justify-between flex-col sm:flex-row gap-2">
@@ -119,57 +113,54 @@ function EducationSection() {
                 North Eeast Scotland College
               </div>
 
+              <div className="flex flex-col gap-2">
+                <div className="flex text-gray-700 flex-row gap-[0.5rem] sm:items-center">
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    className="h-6 w-6"
+                    fill="none"
+                    viewBox="0 0 24 24"
+                    stroke="currentColor"
+                  >
+                    <path d="M12 14l9-5-9-5-9 5 9 5z" />
+                    <path d="M12 14l6.16-3.422a12.083 12.083 0 01.665 6.479A11.952 11.952 0 0012 20.055a11.952 11.952 0 00-6.824-2.998 12.078 12.078 0 01.665-6.479L12 14z" />
+                    <path
+                      strokeLinecap="round"
+                      strokeLinejoin="round"
+                      strokeWidth={2}
+                      d="M12 14l9-5-9-5-9 5 9 5zm0 0l6.16-3.422a12.083 12.083 0 01.665 6.479A11.952 11.952 0 0012 20.055a11.952 11.952 0 00-6.824-2.998 12.078 12.078 0 01.665-6.479L12 14zm-4 6v-7.5l4-2.222"
+                    />
+                  </svg>
+                  <div className="flex flex-wrap gap-2 sm:gap-4">
+                    <span className="text-xl">HND Mechanical Engineering</span>
+                    <span className="text-xl">HNC Electrical Engineering</span>
+                  </div>
+                </div>
 
-<div className="flex flex-col gap-2">
-
-  
-              <div className="flex text-gray-700 flex-row gap-[0.5rem] sm:items-center">
-                <svg
-                  xmlns="http://www.w3.org/2000/svg"
-                  className="h-6 w-6"
-                  fill="none"
-                  viewBox="0 0 24 24"
-                  stroke="currentColor"
-                >
-                  <path d="M12 14l9-5-9-5-9 5 9 5z" />
-                  <path d="M12 14l6.16-3.422a12.083 12.083 0 01.665 6.479A11.952 11.952 0 0012 20.055a11.952 11.952 0 00-6.824-2.998 12.078 12.078 0 01.665-6.479L12 14z" />
-                  <path
-                    strokeLinecap="round"
-                    strokeLinejoin="round"
-                    strokeWidth={2}
-                    d="M12 14l9-5-9-5-9 5 9 5zm0 0l6.16-3.422a12.083 12.083 0 01.665 6.479A11.952 11.952 0 0012 20.055a11.952 11.952 0 00-6.824-2.998 12.078 12.078 0 01.665-6.479L12 14zm-4 6v-7.5l4-2.222"
-                  />
-                </svg>
-              <div className="flex flex-wrap gap-2 sm:gap-4">
-                <span className="text-xl">HND Mechanical Engineering</span>
-                <span className="text-xl">HNC Electrical Engineering</span>
+                <div className="text-gray-700 flex flex-row gap-[0.5rem] items-center">
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    className="h-6 w-6"
+                    fill="none"
+                    viewBox="0 0 24 24"
+                    stroke="currentColor"
+                  >
+                    <path
+                      strokeLinecap="round"
+                      strokeLinejoin="round"
+                      strokeWidth={2}
+                      d="M17.657 16.657L13.414 20.9a1.998 1.998 0 01-2.827 0l-4.244-4.243a8 8 0 1111.314 0z"
+                    />
+                    <path
+                      strokeLinecap="round"
+                      strokeLinejoin="round"
+                      strokeWidth={2}
+                      d="M15 11a3 3 0 11-6 0 3 3 0 016 0z"
+                    />
+                  </svg>
+                  <span className="text-xl">Aberdeen</span>
                 </div>
               </div>
-
-              <div className="text-gray-700 flex flex-row gap-[0.5rem] items-center">
-                <svg
-                  xmlns="http://www.w3.org/2000/svg"
-                  className="h-6 w-6"
-                  fill="none"
-                  viewBox="0 0 24 24"
-                  stroke="currentColor"
-                >
-                  <path
-                    strokeLinecap="round"
-                    strokeLinejoin="round"
-                    strokeWidth={2}
-                    d="M17.657 16.657L13.414 20.9a1.998 1.998 0 01-2.827 0l-4.244-4.243a8 8 0 1111.314 0z"
-                  />
-                  <path
-                    strokeLinecap="round"
-                    strokeLinejoin="round"
-                    strokeWidth={2}
-                    d="M15 11a3 3 0 11-6 0 3 3 0 016 0z"
-                  />
-                </svg>
-                <span className="text-xl">Aberdeen</span>
-              </div>
-            </div>
             </div>
 
             <div className="sm:text-right text-gray-700">
@@ -205,8 +196,7 @@ function EducationSection() {
           src={Gordons}
           alt="Robert Gordons College"
           className="w-[3rem]"
-
-/>
+        />
 
         <div className="w-full py-2 ">
           <div className="flex justify-between flex-col sm:flex-row gap-2">
@@ -271,8 +261,7 @@ function EducationSection() {
           src={Scotch}
           alt="Scotch College WA"
           className="w-[2.5rem]"
-
-/>
+        />
 
         <div className="w-full py-2 ">
           <div className="flex justify-between flex-col sm:flex-row gap-2">
