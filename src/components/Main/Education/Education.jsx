b172cb0e0b9d1ab8afc810da56d3c19f8f4c59d1
ElonMuskRat420xx <gnelson6842@gmail.com>
2023-02-21 12:39:05 +0000

    21st Feb 12:38PM

--- src/components/Main/Education/Education.jsx
+++ src/components/Main/Education/Education.jsx
@@ -2,6 +2,8 @@ import React from "react";
 import Image from "next/image.js";
 import Aberdeen from "../../../../public/assets/Aberdeen.svg";
 import Nescol from "../../../../public/assets/Nescol.svg";
+import Gordons from "../../../../public/assets/Gordons.svg";
+import Scotch from "../../../../public/assets/Scotch.svg";
 
 function EducationSection() {
   return (
@@ -15,23 +17,23 @@ function EducationSection() {
           </h2>
         </div>
       </header>
-
-      <div className="mb-5 ">
+      {/* Education Section University Of Aberdeen  */}
+      <div className="">
         <Image
           priority="true"
           src={Aberdeen}
           alt="Abedeen"
-          className="flex-shrink-0 w-8 h-8 rounded-[2px] bg-cover"
+          className="w-[2rem]"
         />
 
         <div className="w-full py-2 ">
-          <div className="item-header">
+          <div className="flex justify-between flex-col sm:flex-row gap-2">
             <div className="">
               <div className="text-2xl font-medium text-gray-750 py-1">
-                B-Eng Mechanical Engineering
+                University Of Aberdeen
               </div>
-              <div className="flex space-x-5 text-gray-700">
-                <div className="item-header-info">
+              <div className="flex flex-row gap-2 text-gray-700">
+                <div className="flex flex-row gap-[0.5rem] items-center">
                   <svg
                     xmlns="http://www.w3.org/2000/svg"
                     className="h-6 w-6"
@@ -48,9 +50,9 @@ function EducationSection() {
                       d="M12 14l9-5-9-5-9 5 9 5zm0 0l6.16-3.422a12.083 12.083 0 01.665 6.479A11.952 11.952 0 0012 20.055a11.952 11.952 0 00-6.824-2.998 12.078 12.078 0 01.665-6.479L12 14zm-4 6v-7.5l4-2.222"
                     />
                   </svg>
-                  <span className="text-xl">University Of Aberdeen</span>
+                  <span className="text-xl">B-Eng Mechanical Engineering</span>
                 </div>
-                <div className="item-header-info">
+                <div className="flex flex-row gap-[0.5rem] items-center">
                   <svg
                     xmlns="http://www.w3.org/2000/svg"
                     className="h-6 w-6"
@@ -71,12 +73,12 @@ function EducationSection() {
                       d="M15 11a3 3 0 11-6 0 3 3 0 016 0z"
                     />
                   </svg>
-                  <span className="text-xl px-2">Aberdeen</span>
+                  <span className="text-xl">Aberdeen</span>
                 </div>
               </div>
             </div>
             <div className="sm:text-right text-gray-700">
-              <div className="item-header-info">
+              <div className="flex flex-row gap-[0.5rem] items-center">
                 <svg
                   xmlns="http://www.w3.org/2000/svg"
                   className="h-6 w-6"
@@ -98,6 +100,237 @@ function EducationSection() {
           <div className="py-2 border-b border-gray-200" />
         </div>
       </div>
+
+      {/* Education Section NESCOL */}
+
+      <div className="pt-2">
+        <Image
+          priority="true"
+          src={Nescol}
+          alt="NESCOL"
+          className="w-[4rem]"
+
+/>
+
+        <div className="w-full py-2 ">
+          <div className="flex justify-between flex-col sm:flex-row gap-2">
+            <div className="flex flex-col">
+              <div className="text-2xl font-medium text-gray-750 py-1">
+                North Eeast Scotland College
+              </div>
+
+
+<div className="flex flex-col gap-2">
+
+  
+              <div className="flex text-gray-700 flex-row gap-[0.5rem] sm:items-center">
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  className="h-6 w-6"
+                  fill="none"
+                  viewBox="0 0 24 24"
+                  stroke="currentColor"
+                >
+                  <path d="M12 14l9-5-9-5-9 5 9 5z" />
+                  <path d="M12 14l6.16-3.422a12.083 12.083 0 01.665 6.479A11.952 11.952 0 0012 20.055a11.952 11.952 0 00-6.824-2.998 12.078 12.078 0 01.665-6.479L12 14z" />
+                  <path
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    strokeWidth={2}
+                    d="M12 14l9-5-9-5-9 5 9 5zm0 0l6.16-3.422a12.083 12.083 0 01.665 6.479A11.952 11.952 0 0012 20.055a11.952 11.952 0 00-6.824-2.998 12.078 12.078 0 01.665-6.479L12 14zm-4 6v-7.5l4-2.222"
+                  />
+                </svg>
+              <div className="flex flex-wrap gap-2 sm:gap-4">
+                <span className="text-xl">HND Mechanical Engineering</span>
+                <span className="text-xl">HNC Electrical Engineering</span>
+                </div>
+              </div>
+
+              <div className="text-gray-700 flex flex-row gap-[0.5rem] items-center">
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  className="h-6 w-6"
+                  fill="none"
+                  viewBox="0 0 24 24"
+                  stroke="currentColor"
+                >
+                  <path
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    strokeWidth={2}
+                    d="M17.657 16.657L13.414 20.9a1.998 1.998 0 01-2.827 0l-4.244-4.243a8 8 0 1111.314 0z"
+                  />
+                  <path
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    strokeWidth={2}
+                    d="M15 11a3 3 0 11-6 0 3 3 0 016 0z"
+                  />
+                </svg>
+                <span className="text-xl">Aberdeen</span>
+              </div>
+            </div>
+            </div>
+
+            <div className="sm:text-right text-gray-700">
+              <div className="flex flex-row gap-[0.5rem] items-center">
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  className="h-6 w-6"
+                  fill="none"
+                  viewBox="0 0 24 24"
+                  stroke="currentColor"
+                >
+                  <path
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    strokeWidth={2}
+                    d="M8 7V3m8 4V3m-9 8h10M5 21h14a2 2 0 002-2V7a2 2 0 00-2-2H5a2 2 0 00-2 2v12a2 2 0 002 2z"
+                  />
+                </svg>
+                <span className="text-xl">September 2014 – June 2017 </span>
+              </div>
+            </div>
+          </div>
+
+          <div className="py-2 border-b border-gray-200" />
+        </div>
+      </div>
+
+      {/* Education Section RGC */}
+
+      <div className="pt-2">
+        <Image
+          priority="true"
+          src={Gordons}
+          alt="Robert Gordons College"
+          className="w-[3rem]"
+
+/>
+
+        <div className="w-full py-2 ">
+          <div className="flex justify-between flex-col sm:flex-row gap-2">
+            <div className="">
+              <div className="text-2xl font-medium text-gray-750 py-1">
+                Robert Gordons College
+              </div>
+              <div className="flex flex-row gap-2 text-gray-700">
+                <div className="flex flex-row gap-[0.5rem] items-center">
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    className="h-6 w-6"
+                    fill="none"
+                    viewBox="0 0 24 24"
+                    stroke="currentColor"
+                  >
+                    <path
+                      strokeLinecap="round"
+                      strokeLinejoin="round"
+                      strokeWidth={2}
+                      d="M17.657 16.657L13.414 20.9a1.998 1.998 0 01-2.827 0l-4.244-4.243a8 8 0 1111.314 0z"
+                    />
+                    <path
+                      strokeLinecap="round"
+                      strokeLinejoin="round"
+                      strokeWidth={2}
+                      d="M15 11a3 3 0 11-6 0 3 3 0 016 0z"
+                    />
+                  </svg>
+                  <span className="text-xl">Aberdeen</span>
+                </div>
+              </div>
+            </div>
+            <div className="sm:text-right text-gray-700">
+              <div className="flex flex-row gap-[0.5rem] items-center">
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  className="h-6 w-6"
+                  fill="none"
+                  viewBox="0 0 24 24"
+                  stroke="currentColor"
+                >
+                  <path
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    strokeWidth={2}
+                    d="M8 7V3m8 4V3m-9 8h10M5 21h14a2 2 0 002-2V7a2 2 0 00-2-2H5a2 2 0 00-2 2v12a2 2 0 002 2z"
+                  />
+                </svg>
+                <span className="text-xl">November 2009 – June 2014</span>
+              </div>
+            </div>
+          </div>
+          <div className="py-2 border-b border-gray-200" />
+        </div>
+      </div>
+
+      {/* Education Section Scotch College  */}
+      <div className="pt-2">
+        <Image
+          priority="true"
+          src={Scotch}
+          alt="Scotch College WA"
+          className="w-[2.5rem]"
+
+/>
+
+        <div className="w-full py-2 ">
+          <div className="flex justify-between flex-col sm:flex-row gap-2">
+            <div className="">
+              <div className="text-2xl font-medium text-gray-750 py-1">
+                Scotch College
+              </div>
+              <div className="flex flex-row gap-2 text-gray-700">
+                <div className="flex flex-row gap-[0.5rem] items-center">
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    className="h-6 w-6"
+                    fill="none"
+                    viewBox="0 0 24 24"
+                    stroke="currentColor"
+                  >
+                    <path
+                      strokeLinecap="round"
+                      strokeLinejoin="round"
+                      strokeWidth={2}
+                      d="M17.657 16.657L13.414 20.9a1.998 1.998 0 01-2.827 0l-4.244-4.243a8 8 0 1111.314 0z"
+                    />
+                    <path
+                      strokeLinecap="round"
+                      strokeLinejoin="round"
+                      strokeWidth={2}
+                      d="M15 11a3 3 0 11-6 0 3 3 0 016 0z"
+                    />
+                  </svg>
+                  <span className="text-xl">Perth Western Australia</span>
+                </div>
+              </div>
+            </div>
+            <div className="sm:text-right text-gray-700">
+              <div className="flex flex-row gap-[0.5rem] items-center">
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  className="h-6 w-6"
+                  fill="none"
+                  viewBox="0 0 24 24"
+                  stroke="currentColor"
+                >
+                  <path
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    strokeWidth={2}
+                    d="M8 7V3m8 4V3m-9 8h10M5 21h14a2 2 0 002-2V7a2 2 0 00-2-2H5a2 2 0 00-2 2v12a2 2 0 002 2z"
+                  />
+                </svg>
+                <span className="text-xl">November 2007 – June 2009</span>
+              </div>
+            </div>
+          </div>
+          <div className="py-2 border-b border-gray-200" />
+        </div>
+      </div>
+
+      {/* End Of Education Sections */}
     </div>
   );
 }
